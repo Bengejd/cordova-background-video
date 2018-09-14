ee53f16e23328a47f9414a0d66e33c959d6dc322
Jordan Benge <Bengejd@miamioh.edu>
2018-09-14 07:32:46 -0400

    updated maxTime -
    to 10 seconds.

--- src/ios/backgroundvideo.m
+++ src/ios/backgroundvideo.m
@@ -14,9 +14,9 @@
 #pragma mark backgroundvideo
 
 -(void) pluginInitialize{
-  // start as transparent
-  self.webView.opaque = NO;
-  self.webView.backgroundColor = [UIColor clearColor];
+    // start as transparent
+    self.webView.opaque = NO;
+    self.webView.backgroundColor = [UIColor clearColor];
 }
 
 - (void) start:(CDVInvokedUrlCommand *)command
@@ -26,80 +26,75 @@
     //fileStorage, filename, camera, quality
     self.token = [command.arguments objectAtIndex:1];
     self.camera = [command.arguments objectAtIndex:2];
-
+    
     //get rid of the old view (causes issues if the app is resumed)
     self.parentView = nil;
-
+    
     //make the view
-    CGRect viewRect = CGRectMake(
-                                 0,
-                                 0,
-                                 self.webView.superview.frame.size.width,
-                                 self.webView.superview.frame.size.height
-                                 );
-
+    CGRect viewRect = CGRectMake(0, 0, self.webView.superview.frame.size.width, self.webView.superview.frame.size.height);
+    
     self.parentView = [[UIView alloc] initWithFrame:viewRect];
     [self.webView.superview addSubview:self.parentView];
-
+    
     self.view = [[UIView alloc] initWithFrame: self.parentView.bounds];
     [self.parentView addSubview: view];
     self.parentView.userInteractionEnabled = NO;
-
+    
     self.webView.opaque = NO;
     self.webView.backgroundColor = [UIColor clearColor];
     [self.webView.superview bringSubviewToFront:self.webView];
-
+    
     //camera stuff
-
+    
     //Capture session
     session = [[AVCaptureSession alloc] init];
     [session setSessionPreset:AVCaptureSessionPresetMedium];
-
+    
     //Get the front camera and set the capture device
     AVCaptureDevice *inputDevice = [self getCamera: self.camera];
-
-
+    
+    
     //write the file
     outputPath = [self getFileName];
     NSURL *fileURI = [[NSURL alloc] initFileURLWithPath:outputPath];
-
+    
     //capture device output
-    CMTime maxDuration = CMTimeMakeWithSeconds(1800, 1);
-
+    CMTime maxDuration = CMTimeMakeWithSeconds(10, 1); // Changed the limit to be just 10 seconds.
+    
     output = [[AVCaptureMovieFileOutput alloc]init];
     output.maxRecordedDuration = maxDuration;
     output.movieFragmentInterval = kCMTimeInvalid;
-
-
+    
+    
     if ( [session canAddOutput:output])
         [session addOutput:output];
-
+    
     //Capture audio input
     AVCaptureDevice *audioCaptureDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeAudio];
     AVCaptureDeviceInput *audioInput = [AVCaptureDeviceInput deviceInputWithDevice:audioCaptureDevice error:nil];
-
+    
     if ([session canAddInput:audioInput])
         [session addInput:audioInput];
-
+    
     //Capture device input
     AVCaptureDeviceInput *deviceInput = [AVCaptureDeviceInput deviceInputWithDevice:inputDevice error:nil];
     if ( [session canAddInput:deviceInput] )
         [session addInput:deviceInput];
-
-
+    
+    
     //preview view
     self.previewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:session];
     [self.previewLayer setVideoGravity:AVLayerVideoGravityResizeAspectFill];
-
+    
     CALayer *rootLayer = [[self view] layer];
     [rootLayer setMasksToBounds:YES];
     [self.previewLayer setFrame:CGRectMake(0, 0, rootLayer.bounds.size.width, rootLayer.bounds.size.height)];
     [rootLayer insertSublayer:self.previewLayer atIndex:0];
-
+    
     //go
     [session startRunning];
     [output startRecordingToOutputFileURL:fileURI recordingDelegate:self ];
-
+    
     //return true to ensure callback fires
     CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
@@ -109,7 +104,7 @@
 {
     [output stopRecording];
     self.view.alpha = 0;
-
+    
     CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:outputPath];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
@@ -119,14 +114,14 @@
     int fileNameIncrementer = 1;
     NSFileManager *fileManager = [NSFileManager defaultManager];
     NSString *libPath = [self getCachePath];
-
+    
     NSString *tempPath = [[NSString alloc] initWithFormat:@"%@%@%@", libPath, self.token, FileExtension];
-
+    
     while ([fileManager fileExistsAtPath:tempPath]) {
         tempPath = [NSString stringWithFormat:@"%@%@_%i%@", libPath, self.token, fileNameIncrementer, FileExtension];
         fileNameIncrementer++;
     }
-
+    
     return tempPath;
 }
 
